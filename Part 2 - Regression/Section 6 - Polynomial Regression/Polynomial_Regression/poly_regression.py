8330acb3c948f67eac5957b18fe8e278ac0a9066
Unknown <anishamulvaidya@gmail.com>
2018-03-19 23:26:18 +0530

    Data preprocessing, regression & classifications

---
+++ Part 2 - Regression/Section 6 - Polynomial Regression/Polynomial_Regression/poly_regression.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+#plot points to see polynomial regression
+plt.scatter(X, y, color = 'red') 
+
+# Splitting the dataset into the Training set and Test set
+'''from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)'''
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""# -*- coding: utf-8 -*-
+
+#fitting Linear Regression to dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+
+#fitting polynomial regression to dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(X_poly, y)
+
+#visualizing linear regression results
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg.predict(X), color = 'blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+#visualizing polynomial regression results
+X_grid = np.arange(min(X), max(X), 0.1) #smooth curve by 0.1 incrementation value
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, lin_reg2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+#predict new result value with linear regression
+lin_reg.predict(6.5)
+
+#predict new result value with polynomial regression
+lin_reg2.predict(poly_reg.fit_transform(6.5))
---
+++ Part 2 - Regression/Section 9 - Random Forest Regression/Random_Forest_Regression/random_forest_regr.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 11:10:04 2018
+
+@author: anish
+"""
+#import libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#import dataset and variables
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2:3].values
+
+#fitting random forest regression to dataset
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(n_estimators = 300, random_state = 0)
+regressor.fit(X,y)
+
+#predicting a new result
+y_pred = regressor.predict(6.5)
+
+#plot graph(high resolution)
+X_grid = np.arange(min(X), max(X), 0.01)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
+plt.title('Truth or Bluff (Random Forest Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
