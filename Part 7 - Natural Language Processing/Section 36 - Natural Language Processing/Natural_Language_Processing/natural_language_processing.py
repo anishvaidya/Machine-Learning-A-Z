e13b1516469cb2f46d54b460f1d4c2ab4e1373c8
anishvaidya <anishamulvaidya@gmail.com>
2018-04-16 20:11:52 +0530

    nlp and ann

---
+++ Part 7 - Natural Language Processing/Section 36 - Natural Language Processing/Natural_Language_Processing/natural_language_processing.py
@@ -0,0 +1,58 @@
+# Natural Language Processing
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3) # quoting = 3 to ignore ''
+
+# Cleaning the texts
+import re
+import nltk
+nltk.download('stopwords') # Used to remove useless words like 'this' 
+from nltk.corpus import stopwords
+
+# remove no and not from stopwords  
+#from nltk.corpus import stopwords
+stopwords_removed = stopwords.words('english')
+stopwords_removed.remove('not')
+stopwords_removed.remove('no')
+
+
+from nltk.stem.porter import PorterStemmer # to get the root of the word
+corpus = []
+for i in range(0,dataset.shape[0]):
+    review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][i])
+    review = review.lower()
+    review = review.split()
+    ps = PorterStemmer()
+    #review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = [ps.stem(word) for word in review if not word in set(stopwords_removed)]
+    review = ' '.join(review)
+    corpus.append(review)
+
+# Creating the Bag of Words model
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 1500)
+X = cv.fit_transform(corpus).toarray()
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred)
+
+
